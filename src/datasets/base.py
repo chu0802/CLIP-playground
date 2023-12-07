03c7e7bc40088b5765bd99df77dc66513e511b95
chu0802 <r09922104@ntu.edu.tw>
2023-12-07 16:12:09 +0800

    <reorg>: unify dataset architecture

--- src/datasets/base.py
+++ src/datasets/base.py
@@ -13,32 +13,25 @@ def pil_loader(path: str):
 
 
 class BaseClassificationDataset(Dataset):
-    def __init__(self, root, mode="train", transform=None, cache=False):
+    def __init__(self, root, mode="train", transform=None):
         self.root = Path(root) / self.dataset_name
         self.mode = mode
-        self.cache_path = self.root / f".{mode}_cache.json" if cache else None
-        self.data_list, self.class_list = self.make_dataset(cache=cache)
+        self.data_list, self.class_list = self.make_dataset()
         self.transform = transform
 
-    @abstractmethod
-    def _make_dataset(self):
-        raise NotImplementedError
+    def make_dataset(self):
+        with (self.root / self.annotation_filename).open("r") as f:
+            data = json.load(f)
 
-    def make_dataset(self, cache=False):
-        if cache and self.cache_path.exists():
-            with self.cache_path.open("r") as f:
-                data = json.load(f)
-            return data["data_list"], data["class_list"]
-        else:
-            data_list, class_list = self._make_dataset()
-            if cache:
-                self.cache_path.parent.mkdir(parents=True, exist_ok=True)
-                with open(self.cache_path, "w") as f:
-                    json.dump(
-                        {"data_list": data_list, "class_list": class_list}, f, indent=4
-                    )
+        data_list, class_dict = [], {}
+        for d in data[self.mode]:
+            data_list.append(((self.root / "images" / d[0]).as_posix(), d[1]))
+            if d[1] not in class_dict:
+                class_dict[d[1]] = d[2]
 
-            return data_list, class_list
+        class_list = [class_dict[i] for i in sorted(class_dict.keys())]
+
+        return data_list, class_list
 
     def get_class_name(self, class_idx):
         return self.class_list[class_idx]
@@ -54,22 +47,3 @@ class BaseClassificationDataset(Dataset):
             image = self.transform(image)
 
         return image, class_id
-
-
-class BaseCoOPDataset(BaseClassificationDataset):
-    def __init__(self, root, mode="train", transform=None, cache=False):
-        super().__init__(root, mode, transform, cache=cache)
-
-    def _make_dataset(self):
-        with (self.root / self.annotation_filename).open("r") as f:
-            data = json.load(f)
-
-        data_list, class_dict = [], {}
-        for d in data[self.mode]:
-            data_list.append(((self.root / "images" / d[0]).as_posix(), d[1]))
-            if d[1] not in class_dict:
-                class_dict[d[1]] = d[2]
-
-        class_list = [class_dict[i] for i in sorted(class_dict.keys())]
-
-        return data_list, class_list
--- src/datasets/core_dataset.py
+++ src/datasets/core_dataset.py
@@ -1,48 +1,53 @@
-from src.datasets.base import BaseCoOPDataset
+from src.datasets.base import BaseClassificationDataset
 
 
-class Caltech101(BaseCoOPDataset):
+class ImageNet(BaseClassificationDataset):
+    dataset_name = "imagenet"
+    annotation_filename = "annotations.json"
+
+
+class Caltech101(BaseClassificationDataset):
     dataset_name = "caltech-101"
     annotation_filename = "split_zhou_Caltech101.json"
 
 
-class OxfordPets(BaseCoOPDataset):
+class OxfordPets(BaseClassificationDataset):
     dataset_name = "oxford-pets"
     annotation_filename = "split_zhou_OxfordPets.json"
 
 
-class StanfordCars(BaseCoOPDataset):
+class StanfordCars(BaseClassificationDataset):
     dataset_name = "stanford-cars"
     annotation_filename = "split_zhou_StanfordCars.json"
 
 
-class Flowers102(BaseCoOPDataset):
+class Flowers102(BaseClassificationDataset):
     dataset_name = "flowers-102"
     annotation_filename = "split_zhou_OxfordFlowers.json"
 
 
-class Food101(BaseCoOPDataset):
+class Food101(BaseClassificationDataset):
     dataset_name = "food-101"
     annotation_filename = "split_zhou_Food101.json"
 
 
-class FGVCAircraft(BaseCoOPDataset):
+class FGVCAircraft(BaseClassificationDataset):
     dataset_name = "fgvc-aircraft"
     annotation_filename = "annotations.json"
 
 
-class EuroSAT(BaseCoOPDataset):
+class EuroSAT(BaseClassificationDataset):
     dataset_name = "eurosat"
     annotation_filename = "split_zhou_EuroSAT.json"
 
 
-class UCF101(BaseCoOPDataset):
+class UCF101(BaseClassificationDataset):
     dataset_name = "ucf-101"
     annotation_filename = "split_zhou_UCF101.json"
 
 
 if __name__ == "__main__":
-    dataset = UCF101(root="/mnt/data/classification", mode="test")
+    dataset = ImageNet(root="/mnt/data/classification", mode="val")
     print(dataset[0])
     print(len(dataset))
     print(len(dataset.class_list))
