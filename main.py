1b763a0c28a64f5c64c02182f7d3d325ff7bb4cf
chu0802 <r09922104@ntu.edu.tw>
2023-12-08 08:14:32 +0800

    <update>: update main example to new model arch

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from omegaconf import OmegaConf
 from tqdm import tqdm
 
-from src.datasets import Flowers102, ImageNet
-from src.datasets.transform import load_model_transform
+from src.datasets import ImageNet
+from src.datasets.transform import load_transform
 from src.datasets.utils import build_dataloader
-from src.models.clip import ClipModel
+from src.models.clip import load_model
 
 
 def main(config):
-    model = ClipModel(config.model)
-    model.clip_base.cuda()
-    model.clip_base.eval()
-
-    _, eval_transform = load_model_transform(model.clip_base)
+    _, eval_transform = load_transform()
 
     dataset = ImageNet(
         config.data.root,
@@ -21,16 +17,15 @@ def main(config):
         sample_num=config.data.sample_num,
     )
 
-    dataloader = build_dataloader(dataset, batch_size=64)
+    model = load_model(config.model, dataset.class_name_list, device="cuda")
+    dataloader = build_dataloader(dataset, batch_size=512)
 
     correct, total = 0, 0
     for images, labels in tqdm(dataloader):
         images = images.cuda()
         labels = labels.cuda()
 
-        probs = model.inference(images, dataset.class_list)
-
-        preds = probs.argmax(dim=1)
+        preds = model(images).argmax(dim=1)
 
         correct += sum(preds == labels).item()
         total += len(preds)
