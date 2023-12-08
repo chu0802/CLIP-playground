3f2267591f4bd40291344a55ecfea74793010090
chu0802 <r09922104@ntu.edu.tw>
2023-12-08 08:13:45 +0800

    <update>: add default preprocessing config

--- src/datasets/transform.py
+++ src/datasets/transform.py
@@ -1,13 +1,28 @@
+from open_clip.constants import OPENAI_DATASET_MEAN, OPENAI_DATASET_STD
 from open_clip.transform import PreprocessCfg, image_transform_v2
 
+DEFAULT_PREPROCESS_CONFIG = {
+    "size": (224, 224),
+    "mode": "RGB",
+    "mean": OPENAI_DATASET_MEAN,
+    "std": OPENAI_DATASET_STD,
+    "interpolation": "bicubic",
+    "resize_mode": "shortest",
+    "fill_color": 0,
+}
 
-def load_model_transform(model_preprocess_config):
+
+def load_transform(model_preprocess_config=None):
+    if model_preprocess_config is None:
+        model_preprocess_config = DEFAULT_PREPROCESS_CONFIG
+
     pp_cfg = PreprocessCfg(**model_preprocess_config)
 
     train_transform = image_transform_v2(
         pp_cfg,
         is_train=True,
     )
+
     eval_transform = image_transform_v2(
         pp_cfg,
         is_train=False,
