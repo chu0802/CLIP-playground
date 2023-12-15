1977c16b8cdf391b2da5df9b60d80f22ca736547
chu0802 <r09922104@ntu.edu.tw>
2023-12-15 18:32:04 +0800

    <feat>: add a script to auto continually training

---
+++ continually_train.py
@@ -0,0 +1,55 @@
+import subprocess
+from pathlib import Path
+
+DATASET_SEQ = [
+    "fgvc-aircraft",
+    "caltech-101",
+    "dtd",
+    "eurosat",
+    "flowers-102",
+    "oxford-pets",
+    "stanford-cars",
+    "ucf-101",
+]
+OUTPUTS_ROOT = Path("outputs/ViT-B-16")
+
+
+def start_subprocess(command, print_command=False):
+    if print_command:
+        print(" ".join(command) + "\n")
+
+    process = subprocess.Popen(command)
+    return process.wait()
+
+
+if __name__ == "__main__":
+    pretrained_path = "openai"
+    wise_path = "openai"
+
+    for i, dataset in enumerate(DATASET_SEQ):
+        # training
+        commands = [
+            "python",
+            "kd_train.py",
+            "--options",
+            f"data.name={dataset}",
+            f"model.pretrained={pretrained_path}",
+            f"model.wise_path={wise_path}",
+        ]
+        start_subprocess(commands, print_command=True)
+
+        # evaluation
+        pretrained_path = OUTPUTS_ROOT / dataset / "latest" / "checkpoint_10.pth"
+
+        for eval_dataset in DATASET_SEQ:
+            commands = [
+                "python",
+                "evaluate.py",
+                "--options",
+                f"data.name={eval_dataset}",
+                f"model.pretrained={pretrained_path}",
+                f"model.wise_path={wise_path}",
+            ]
+            start_subprocess(commands, print_command=False)
+
+        wise_path = pretrained_path
