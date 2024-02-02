82c35fc7b1af819062470d118d1398008fc9a0b8
chu0802 <r09922104@ntu.edu.tw>
2024-02-02 11:41:46 +0800

    <feat>: add mix teacher feature

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -367,15 +367,44 @@ class MixTeacherKDTrainer(ZSCLTrainer):
     def prev_teacher_model(self):
         return self._teachers["prev"]
 
+    def scoring_function(
+        self, pretrained_teacher_logits, prev_teacher_logits, threshold=0.2, scale=6
+    ):
+        pre_scores = torch.norm(pretrained_teacher_logits - prev_teacher_logits, dim=-1)
+        self.num_valid_prev_data += (pre_scores > threshold).float().sum().item()
+
+        scaled_scores = scale * (pre_scores - threshold)
+
+        return nn.functional.sigmoid(scaled_scores).reshape(-1, 1)
+
+    def get_mix_teacher_feature(
+        self,
+        pretrained_teacher_logits,
+        prev_teacher_logits,
+        threshold=0.2,
+        scale=6,
+        ratio_prev=9,
+        ratio_pretrained=0.5,
+    ):
+        scores = self.scoring_function(
+            pretrained_teacher_logits, prev_teacher_logits, threshold, scale
+        )
+        mix_teacher_feature = (
+            ratio_pretrained * (1 - scores) * pretrained_teacher_logits
+            + ratio_prev * scores * prev_teacher_logits
+        )
+
+        return mix_teacher_feature
+
     def mix_teacher_loss(
         self,
         images,
         labels,
         mixup=True,
-        ratio_prev=5,
+        ratio_prev=9,
         ratio_pretrained=0.5,
-        threshold=0.25,
-        scale=10,
+        threshold=0.2,
+        scale=6,
     ):
         ref_images, _ = self.get_ref_data(self.ref_loader)
         base_loss = self.base_loss(images, labels)
@@ -395,40 +424,28 @@ class MixTeacherKDTrainer(ZSCLTrainer):
             )
             prev_teacher_logits = self.prev_teacher_model.get_features(ref_images)
 
-        pre_scores = torch.norm(pretrained_teacher_logits - prev_teacher_logits, dim=-1)
-
-        self.num_valid_prev_data += (pre_scores > threshold).float().sum().item()
-
-        scaled_scores = scale * (pre_scores - threshold)
-
-        scores = nn.functional.sigmoid(scaled_scores).reshape(-1, 1)
-
-        pretrained_kd_loss = self._get_kd_loss(
-            student_logits,
+        mix_teacher_feature = self.get_mix_teacher_feature(
             pretrained_teacher_logits,
-            feature_criterion=self.feature_criterion,
-        )
-        prev_kd_loss = self._get_kd_loss(
-            student_logits,
             prev_teacher_logits,
-            feature_criterion=self.feature_criterion,
+            threshold,
+            scale,
+            ratio_prev,
+            ratio_pretrained,
         )
 
-        prev_kd_loss = (scores * prev_kd_loss).mean()
+        mix_teacher_loss = torch.sqrt(
+            self._get_kd_loss(
+                student_logits,
+                mix_teacher_feature,
+                feature_criterion=self.feature_criterion,
+            )
+        ).mean()
 
-        pretrained_kd_loss = ((1 - scores) * pretrained_kd_loss).mean()
-
-        return (
-            base_loss
-            + ratio_prev * prev_kd_loss
-            + ratio_pretrained * pretrained_kd_loss,
-            {
-                "base_loss": base_loss.item(),
-                "prev_kd_loss": prev_kd_loss.item(),
-                "pretrained_kd_loss": pretrained_kd_loss.item(),
-                "num_valid_prev_data": self.num_valid_prev_data,
-            },
-        )
+        return base_loss + mix_teacher_loss, {
+            "base_loss": base_loss.item(),
+            "mix_teacher_loss": mix_teacher_loss.item(),
+            "num_valid_prev_data": self.num_valid_prev_data,
+        }
 
 
 def get_kd_trainer(model, dataloaders, config, teacher_models):
