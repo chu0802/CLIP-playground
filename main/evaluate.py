abd5d4b71ec1bd4b76af4a04a39d9430d5e79812
chu0802 <r09922104@ntu.edu.tw>
2024-02-08 03:13:11 +0800

    <update>: fix bug for eval after supporting dist training

--- main/evaluate.py
+++ main/evaluate.py
@@ -1,5 +1,5 @@
 from src.datasets.utils import get_dataloaders_from_config
-from src.models.clip import get_model
+from src.models.clip import load_model_from_pretrained
 from src.trainer import BaseTrainer as Trainer
 from src.utils import get_config, setup_seeds
 
@@ -7,7 +7,7 @@ from src.utils import get_config, setup_seeds
 def main(config):
     setup_seeds(config.task.seed)
 
-    model = get_model(config, device="cuda")
+    model = load_model_from_pretrained(config, device="cuda", freeze=True)
 
     dataloaders = get_dataloaders_from_config(config)
 
