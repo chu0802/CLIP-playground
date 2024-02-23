5a3c40678a73ba5e32f26cfb1c2b63b30f39e8a5
chu0802 <r09922104@ntu.edu.tw>
2024-02-23 17:51:36 +0800

    <update>: parse result for mdcil

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -3,7 +3,7 @@ import json
 
 import pandas as pd
 
-from scripts.utils import DEFAULT_DATASET_SEQ, DEFAULT_OUTPUT_ROOT
+from scripts.utils import DEFAULT_DATASET_SEQ, DEFAULT_STORAGE_ROOT
 
 DEFAULT_ZERO_SHOT_PERFORMANCE = {
     "fgvc-aircraft": 0.2391,
@@ -11,6 +11,7 @@ DEFAULT_ZERO_SHOT_PERFORMANCE = {
     "dtd": 0.4439,
     "eurosat": 0.4222,
     "flowers-102": 0.6740,
+    "food-101": 0.84,
     "oxford-pets": 0.8727,
     "stanford-cars": 0.6551,
     "ucf-101": 0.6426,
@@ -24,7 +25,9 @@ def metric_to_dataframe(metric, index_name, columns=DEFAULT_DATASET_SEQ):
 
 def max_catastrophic_forgetting(res_list):
     metric = {
-        res.index[0]: 100 * (res.iloc[:, 0].min() - res.iloc[:, 0].max())
+        res.index[0]: 100
+        * (res.loc[:, res.index[0]].min() - res.loc[:, res.index[0]].max())
+        # res.index[0]: 100 * res.loc[:, res.index[0]].min()
         for res in res_list
     }
     return metric_to_dataframe(metric, "catastrophic forgetting")
@@ -33,7 +36,11 @@ def max_catastrophic_forgetting(res_list):
 def max_zero_shot_degradation(res_list):
     metric = {
         res.index[-1]: 100
-        * (res.iloc[:, -1].min() - DEFAULT_ZERO_SHOT_PERFORMANCE[res.index[-1]])
+        * (
+            res.loc[:, res.index[-1]].min()
+            - DEFAULT_ZERO_SHOT_PERFORMANCE[res.index[-1]]
+        )
+        # res.index[-1]: 100 * res.loc[:, res.index[-1]].min()
         for res in res_list
     }
     return metric_to_dataframe(metric, "zero-shot degradation")
@@ -46,11 +53,20 @@ def avg_final_performance(res_list):
     return metric.to_frame("avg. final performance").T
 
 
-def parse_results(method_dir="split_teacher_config"):
+def parse_results(method="split_teacher_pure_clip"):
+    if "mdcil" in method:
+        config_name = f'{"_".join(method.split("_")[:-1])}_config'
+    else:
+        config_name = f"{method}_config"
     res_list = []
     for order in range(8):
         res_path = (
-            DEFAULT_OUTPUT_ROOT / f"order_{order}" / method_dir / "final_results.json"
+            DEFAULT_STORAGE_ROOT
+            / method
+            / "outputs"
+            / f"order_{order}"
+            / config_name
+            / "final_results.json"
         )
         with res_path.open("r") as f:
             res = json.load(f)
@@ -59,7 +75,7 @@ def parse_results(method_dir="split_teacher_config"):
 
 
 def main(args):
-    res_list = parse_results(method_dir=f"{args.method}_config")
+    res_list = parse_results(method=args.method)
 
     if args.order == "overall":
         forget = max_catastrophic_forgetting(res_list)
@@ -68,12 +84,16 @@ def main(args):
 
         print(pd.concat([forget, degradation, avg], axis=0).round(2))
     else:
-        print((100 * res_list[int(args.order)]).round(2))
+        print(
+            (100 * res_list[int(args.order)])
+            .round(2)
+            .loc[:, res_list[int(args.order)].index]
+        )
 
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
-    p.add_argument("--method", default="split_teacher")
+    p.add_argument("--method", default="split_teacher_pure_clip")
     p.add_argument(
         "--order",
         type=str,
