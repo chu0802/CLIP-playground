f5513d1f457ebf66906b205eb4669411f7c1f187
chu0802 <r09922104@ntu.edu.tw>
2024-02-24 16:54:51 +0800

    add a script for vanilla training

---
+++ main/vanilla_train.py
@@ -0,0 +1,43 @@
+from copy import deepcopy
+
+from src.datasets.utils import get_dataloaders_from_config, load_class_name_list
+from src.models.clip import get_model
+from src.trainer import BaseTrainer
+from src.utils import (
+    get_config,
+    get_job_id,
+    init_distributed_mode,
+    is_main_process,
+    setup_seeds,
+    wandb_logger,
+)
+
+
+@wandb_logger
+def main(config):
+    job_id = get_job_id() if is_main_process() else None
+    setup_seeds(config.task.seed)
+
+    class_name_list, num_classes_accumulation_dict = load_class_name_list(config)
+
+    model = get_model(
+        config, class_name_list, device="cuda", freeze=False, pretrained=False
+    )
+
+    dataloaders = get_dataloaders_from_config(config, num_classes_accumulation_dict)
+
+    trainer = BaseTrainer(model, dataloaders, config, job_id)
+
+    # validation provides nearly no information in my experiments
+    trainer.train(set_validation=False)
+
+    trainer.logging(
+        local_desc="fine-tuned", test_acc=trainer.evaluate(trainer.test_loader)
+    )
+    trainer.dump_results()
+
+
+if __name__ == "__main__":
+    config = get_config(mode="train")
+    # init_distributed_mode(config.task)
+    main(config)
